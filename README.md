556a88a92727542681afaa79efe5d80f58b2660f
george <gigwegbe@gmail.com>
2021-07-26 05:10:07 +0100

    Remove reductant commits

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Person Detection with Esp32
 
 Deploying Person Detection on ESP32 on ESP32 3.5" TFT Touch(Capacitive) board using TFLite. The dataset is from the Visual Wake Words Challenge 2019.
+
+## Setup
+- Arduino IDE
+- Tensorflow 
--- person_detection_ESP32-Camera/app_camera_esp.c
+++ person_detection_ESP32-Camera/app_camera_esp.c
@@ -33,31 +33,7 @@ int app_camera_init() {
   gpio_config(&conf);
 #endif
   camera_config_t config;
-//  config.ledc_channel = LEDC_CHANNEL_0;
-//  config.ledc_timer = LEDC_TIMER_0;
-//  config.pin_d0 = Y2_GPIO_NUM;
-//  config.pin_d1 = Y3_GPIO_NUM;
-//  config.pin_d2 = Y4_GPIO_NUM;
-//  config.pin_d3 = Y5_GPIO_NUM;
-//  config.pin_d4 = Y6_GPIO_NUM;
-//  config.pin_d5 = Y7_GPIO_NUM;
-//  config.pin_d6 = Y8_GPIO_NUM;
-//  config.pin_d7 = Y9_GPIO_NUM;
-//  config.pin_xclk = XCLK_GPIO_NUM;
-//  config.pin_pclk = PCLK_GPIO_NUM;
-//  config.pin_vsync = VSYNC_GPIO_NUM;
-//  config.pin_href = HREF_GPIO_NUM;
-//  config.pin_sscb_sda = SIOD_GPIO_NUM;
-//  config.pin_sscb_scl = SIOC_GPIO_NUM;
-//  config.pin_pwdn = PWDN_GPIO_NUM;
-//  config.pin_reset = -1;  // RESET_GPIO_NUM;
-//  config.xclk_freq_hz = XCLK_FREQ;
-//  config.pixel_format = CAMERA_PIXEL_FORMAT;
-//  config.frame_size = CAMERA_FRAME_SIZE;
-//  config.jpeg_quality = 10;
-//  config.fb_count = 1;
-//
-//
+
 
   config.ledc_channel = LEDC_CHANNEL_0;
   config.ledc_timer = LEDC_TIMER_0;
